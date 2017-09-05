07c8b184afab9e26d4bca2d7ad8a556a81329246
miyake <wmiyake@gmail.com>
2017-09-05 15:48:54 -0300

    Primeiro Commit

---
+++ Calculos/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero;
+            double total;
+            string operacao;
+
+            total = 0;
+            operacao = "+";
+
+            while (operacao != "=")
+            {
+                Console.Write("Entre com o valor: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+                switch (operacao)
+                {
+                    case "+":
+                        total = total + numero;
+                        break;
+                    case "-":
+                        total = total - numero;
+                        break;
+                    case "*":
+                        total = total * numero;
+                        break;
+                    case "/":
+                        total = total / numero;
+                        break;
+
+                }
+
+
+                Console.Write("Digite a operaçâo: ");
+                operacao = Console.ReadLine();
+
+
+            }
+
+            Console.WriteLine("O resultado é : " + total);
+            Console.ReadKey();
+
+        }
+    }
+}
